98210aa4ab420c423e127ee266a243eead08ed45
slovak <slava_muhin@tut.by>
2019-07-13 22:49:16 +0300

    Task 1.6: Use ProductDao on PLP

--- src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
+++ src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,6 +25,8 @@ public class ProductListPageServletTest {
     private HttpServletResponse response;
     @Mock
     private RequestDispatcher requestDispatcher;
+    @Mock
+    private ServletConfig servletConfig;
 
     private ProductListPageServlet servlet = new ProductListPageServlet();
 
@@ -34,8 +37,8 @@ public class ProductListPageServletTest {
 
     @Test
     public void testDoGet() throws ServletException, IOException {
+        servlet.init(servletConfig);
         servlet.doGet(request, response);
-
         verify(requestDispatcher).forward(request, response);
     }
 }
